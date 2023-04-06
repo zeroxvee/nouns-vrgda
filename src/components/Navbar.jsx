c4333b970e8dc27d793a72a88b6cc00bd4dbf8c9
Vlad B <vladimir.bazhutin@gmail.com>
2023-04-06 13:01:03 -0400

    thirdweb wallet connect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
-import { ConnectKitButton } from "connectkit"
-import Link  from "next/link"
+import Link from "next/link"
 import Image from "next/image"
 import logo from "@/assets/logo.svg"
+import { ConnectWallet } from "@thirdweb-dev/react"
 
 export default function Navbar({ className, ...rest }) {
     return (
         <header className="bg-gray-200 py-12">
             <div className="w-[80vw] mx-auto flex items-center justify-between md:px-32 px-4">
                 <Link href="/">
-                    <Image src="/noggles.7644bfd0.svg" width="100" height="40" alt="Nouns DAO noggles"/>
+                    <Image
+                        src="/noggles.7644bfd0.svg"
+                        width="100"
+                        height="40"
+                        alt="Nouns DAO noggles"
+                    />
                 </Link>
-                <ConnectKitButton />
+                <ConnectWallet />
             </div>
         </header>
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,10 @@
 import "@/styles/globals.css"
-import { WagmiConfig, createClient, goerli, chain } from "wagmi"
-import { ConnectKitProvider, getDefaultClient } from "connectkit"
-import { ThirdwebProvider, useContract } from "@thirdweb-dev/react";
-
-const alchemyUrl = process.env.ALCHEMY_URL
-const chains = [goerli]
-
-const client = createClient(
-    getDefaultClient({
-        appName: "nouns-vrgda",
-        alchemyUrl,
-        chains: chains,
-    })
-)
+import { ThirdwebProvider} from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }) {
     return (
         <ThirdwebProvider activeChain="goerli">
-            <WagmiConfig client={client}>
-                <ConnectKitProvider>
                     <Component {...pageProps} />
-                </ConnectKitProvider>
-            </WagmiConfig>
         </ThirdwebProvider>
 
     )
