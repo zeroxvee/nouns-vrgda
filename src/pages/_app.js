e469a7185f447ec42a4d44e20fc21440bfc486ca
vbazhutin <vladimir.bazhutin@gmail.com>
2023-04-09 01:18:55 -0400

    Context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.css"
 import { ThirdwebProvider } from "@thirdweb-dev/react"
+import { AuctionProvider } from "../context/AuctionContext"
 
 export default function App({ Component, pageProps }) {
     return (
         <ThirdwebProvider activeChain="goerli">
-            <Component {...pageProps} />
+            <AuctionProvider>
+                <Component {...pageProps} />
+            </AuctionProvider>
         </ThirdwebProvider>
     )
 }
