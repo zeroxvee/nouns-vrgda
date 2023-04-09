468ecf530ea29616b8852a3d617ad4fcee56ad6b
vbazhutin <vladimir.bazhutin@gmail.com>
2023-04-08 23:12:37 -0400

    AuctionPrice: conditional price render

--- src/components/AuctionPrice/index.js
+++ src/components/AuctionPrice/index.js
@@ -1,18 +1,34 @@
-import { useContract, useContractRead } from "@thirdweb-dev/react";
-import { utils } from 'ethers';
+import { useContract, useContractRead } from "@thirdweb-dev/react"
+import { ethers, utils } from "ethers"
 
 export default function AuctionDetails() {
-    const { contract } = useContract("0xaF71644feEAf6439015D57631f59f8e0E0F91C67");
-    const { data: currVRGDAPrice } = useContractRead(contract, "getCurrentVRGDAPrice");
-    const currFakePrice = 0.055;
-
-    // console.log("currVRGDAPrice", currVRGDAPrice);
+    const { contract } = useContract(
+        "0xaF71644feEAf6439015D57631f59f8e0E0F91C67"
+    )
+    const { data: currentPrice, isLoading: loadingCurr } = useContractRead(
+        contract,
+        "getCurrentVRGDAPrice"
+    )
+    const { data: minPrice, isLoading: loadingMin } = useContractRead(
+        contract,
+        "reservePrice"
+    )
+    console.log("minPrice", minPrice)
+    console.log("currentPrice", currentPrice)
+    const minMintPrice = loadingMin ? "" : ethers.utils.formatEther(minPrice)
+    const currMintrice = loadingCurr
+        ? ""
+        : ethers.utils.formatEther(currentPrice)
 
     return (
         <div className="w-full">
             <h2 className="text-[#1C2228] text-3xl font-['PT Sans']">
-                Ξ {currFakePrice}
-                {/* Ξ {currVRGDAPrice && Number(utils.formatEther(currVRGDAPrice.toString())).toFixed(3)} */}
+                Ξ
+                {currMintrice > minMintPrice
+                    ? Number(
+                          utils.formatEther(currVRGDAPrice.toString())
+                      ).toFixed(3)
+                    : minMintPrice}
             </h2>
         </div>
     )
